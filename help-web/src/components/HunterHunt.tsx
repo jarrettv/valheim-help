75ddc09ee9fc2d82600fc1fc2ea751c6a64d05f9
Jarrett Vance <jarrettv@gmail.com>
2024-08-18 15:42:29 -0500

    fixes

--- help-web/src/components/HunterHunt.tsx
+++ help-web/src/components/HunterHunt.tsx
@@ -61,6 +61,7 @@ export default function HunterHunt() {
     setLoading('');
   };
   
+  
   const calculateScore = (updatedHunt: TrophyHunt) => {
     const selectedTrophies = trophies.filter((trophy) =>
       updatedHunt.trophies.includes(trophy.name)
@@ -110,7 +111,7 @@ export default function HunterHunt() {
           <div>2024-08-16 Hunt</div>
           <button className='discord' style={{ display: 'flex', gap: '1rem' }} onClick={register}>
             <div>Register</div>
-            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 256 199">
+            <svg width="22" height="22" viewBox="0 0 256 199">
               <path fill="#fff" d="M216.856 16.597A208.5 208.5 0 0 0 164.042 0c-2.275 4.113-4.933 9.645-6.766 14.046q-29.538-4.442-58.533 0c-1.832-4.4-4.55-9.933-6.846-14.046a207.8 207.8 0 0 0-52.855 16.638C5.618 67.147-3.443 116.4 1.087 164.956c22.169 16.555 43.653 26.612 64.775 33.193A161 161 0 0 0 79.735 175.3a136.4 136.4 0 0 1-21.846-10.632a109 109 0 0 0 5.356-4.237c42.122 19.702 87.89 19.702 129.51 0a132 132 0 0 0 5.355 4.237a136 136 0 0 1-21.886 10.653c4.006 8.02 8.638 15.67 13.873 22.848c21.142-6.58 42.646-16.637 64.815-33.213c5.316-56.288-9.08-105.09-38.056-148.36M85.474 135.095c-12.645 0-23.015-11.805-23.015-26.18s10.149-26.2 23.015-26.2s23.236 11.804 23.015 26.2c.02 14.375-10.148 26.18-23.015 26.18m85.051 0c-12.645 0-23.014-11.805-23.014-26.18s10.148-26.2 23.014-26.2c12.867 0 23.236 11.804 23.015 26.2c0 14.375-10.148 26.18-23.015 26.18" />
             </svg>
             <strong>{user.user_metadata?.custom_claims?.global_name}</strong>
@@ -129,18 +130,14 @@ export default function HunterHunt() {
             <div><Spinner status={loading} /></div>
           </div>
           <div className="trophies">
-            {groups.map((group) => (
-              <>
-                  {trophies.filter(t => t.group === group).map((trophy) => (
-                    <TrophyOption key={trophy.name} name={trophy.name} imgSrc={trophy.image.src} score={trophy.score} isSelected={hunt.trophies.includes(trophy.name)} onSelect={() => {
-                      if (hunt.trophies.includes(trophy.name)) {
-                        updateHunt({...hunt, trophies: hunt.trophies.filter(t => t !== trophy.name)});
-                      } else {
-                        updateHunt({...hunt, trophies: [...hunt.trophies, trophy.name]});
-                      }
-                    }} />
-                  ))}
-              </>
+            {trophies.sort((a, b) => groups.indexOf(a.group) - groups.indexOf(b.group)).map((trophy) => (
+              <TrophyOption key={trophy.name} name={trophy.name} imgSrc={trophy.image.src} score={trophy.score} isSelected={hunt.trophies.includes(trophy.name)} onSelect={() => {
+                if (hunt.trophies.includes(trophy.name)) {
+                  updateHunt({...hunt, trophies: hunt.trophies.filter(t => t !== trophy.name)});
+                } else {
+                  updateHunt({...hunt, trophies: [...hunt.trophies, trophy.name]});
+                }
+              }} />
             ))}
           </div>
         </div>
