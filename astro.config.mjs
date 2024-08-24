be83c969ef993dc8462946d53c8eef21784e115e
Jarrett Vance <jarrettv@gmail.com>
2024-08-24 13:29:50 -0500

    azure func

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
   site: "https://valheim.help",
   integrations: [react()],
-  adapter: node({
-    mode: 'standalone'
-  })
 });
